4621feb103747f1f8b64be88f29d526d7801ecd3
Faizila Amlani <amlani.faizila@gmail.com>
2021-10-12 16:14:49 -0500

    added packages needed for this application and array of questions for user input in index.js

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
-// TODO: Include packages needed for this application
+//packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+const generate = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'project',
+    },
+    {
+        type: 'input',
+        message: 'Write a brief description of your project: ',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions if any: ',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'Enter project usage information: ',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Enter the project license if any: ',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'Who are the contributors of this project? ',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Enter test instructions: ',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username: ',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email address: ',
+        name: 'email',
+    }
+    
+];
 
-// TODO: Create a function to write README file
+//function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
+//function to initialize app
 function init() {}
 
-// Function call to initialize app
+//function call to initialize app
 init();
